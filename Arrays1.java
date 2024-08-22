78c8e58611db8249842baeda8d7657f77874fc6d
Sanjay-U205 <su8446182@gmail.com>
2024-08-22 21:41:31 +0530

    Arrays1.java

--- Arrays1.java
+++ Arrays1.java
@@ -18,3 +18,19 @@ class Arrays{
 
     }
 }
+//the following code shoes the different ways that an array can be declared and initailized
+public classic ArrayDefinitionDemo{
+    public static void main(String args[]){
+        float x[];
+        x = new float[100];
+        args = new String[10];
+        boolean[] isPrime = new boolean[1000];
+        int fib[] = {0,1,1,2,3,5,8,13};
+        short[][][] b = new short[4][10][5];
+        double a[][] = {{1.1,2.2},{3.3,4.4},null,{5.5,6.6},null};
+        a[4] = new double[66];
+        a[4][65] = 3.14;
+        object[] objects = {x,args,isprime,fib,b,a};
+    }
+}
+    
